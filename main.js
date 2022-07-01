2f651b892994cfa894e28ed80cfbb6f94d641b15
Leon-Is-Bad-At-Coding <84482670+Leon-Is-Bad-At-Coding@users.noreply.github.com>
2022-06-30 21:18:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("#square_kern").load("demos/square_kern.html");
+});
+
+$(function(){
+  $("#double_square").load("demos/double_square.html");
+});
+
+$(function(){
+  $("#circle_kern").load("demos/circle_kern.html");
+});
